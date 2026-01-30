13b04c23691943130c03be0c25b22777d8bc2c08
Ramadan792 <yusuframadan792@gmail.com>
2026-01-30 21:11:40 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js for Vercel
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ace-motion-pictures",
+  base: '/AceMotionPictures/', // --- IGNORE ---
 })
