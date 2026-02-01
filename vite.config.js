6af9c0fb7614cf8cd768d4301f4a8c779547b7fd
Ramadan792 <yusuframadan792@gmail.com>
2026-02-01 10:55:23 +0100

    Fix: Update Vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js for Vercel
+// vite.config.js - For Vercel
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AceMotionPictures', // --- IGNORE ---
+  // base: '/AceMotionPictures/' // REMOVE or COMMENT OUT this line
 })
